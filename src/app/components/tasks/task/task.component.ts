e02a00027752c095ccea707b8eb770adc05a3af8
Abdelrhman Ehab <abdelrhmanehab047@gmail.com>
2025-02-26 11:22:45 +0200

    chore: Update application title to "Easy Task Management"
    - Change HTML title tag in index.html for better app identification

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
 import { Task } from '../tasks.model';
 import { CardComponent } from '../../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
